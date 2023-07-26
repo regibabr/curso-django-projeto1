e96f7376108ee069636382686e6417438a206f0e
regibabr <regibabr@hotmail.com>
2023-07-26 08:34:57 -0300

    base_templates

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes\home.html')
 
 def contato(request):
     return HttpResponse('Contato')
