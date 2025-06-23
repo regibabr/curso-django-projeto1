948cf8953ab46e1fa387c72ece26b99fe3c325fd
Regimilson Oliveira da Silva <regibabr@hotmail.com>
2025-06-23 18:02:59 -0300

    htmls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.http import HttpResponse
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 
 
@@ -8,6 +8,6 @@ from recipes.views import home, contato, sobre
 urlpatterns = [
     
     path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+   # path('contato/', contato),
+   # path('sobre/', sobre),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={'name': 'Luiz Otávio',})
 
-def contato(request):
-    return render(request, 'me-apague/temp.html')
-
-def sobre(Request):
-    return HttpResponse('sobre')
